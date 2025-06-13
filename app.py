f20f591ccc75d01260c110498e36e092cae50fe8
Gauthier PATARD <patard.g@gmail.com>
2025-06-13 13:22:18 +0200

    Background update

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ UPLOAD_TEMPLATE = '''
 }
         
         .container {
-            background: white;
+            background: red;
             border-radius: 20px;
             padding: 40px 30px;
             box-shadow: 0 20px 40px rgba(0,0,0,0.1);
